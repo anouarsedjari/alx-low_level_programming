b258cc554f970e5810c9139e40b5b074589cf0d3
anouar sedjari <anouarsedjari@outlook.com>
2023-06-21 01:19:41 +0100

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include "nain.h"
+#include "main.h"
 /**
 *print_alphabet_x10 - Pribt the alphabet in lowercase.
 *
@@ -8,6 +8,7 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char c;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
