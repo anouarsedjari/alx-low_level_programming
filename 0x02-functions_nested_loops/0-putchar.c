5793094ae462721cb2cddb115f26d39ac1856a50
anouar sedjari <anouarsedjari@outlook.com>
2023-06-21 01:08:10 +0100

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
