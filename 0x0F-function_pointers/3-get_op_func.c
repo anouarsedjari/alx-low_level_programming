55b1b743646546879003c4317d6dfca9005d7739
anouar sedjari <anouarsedjari@outlook.com>
2023-07-22 01:59:10 +0100

    c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,8 @@
 #include "3-calc.h"
 #include <stdlib.h>
 /**
- * get_op_func - Selects the correct function to perform the operation asked by the user
+ * get_op_func - Selects the correct function to perform
+ * the operation asked by the user
  * @s: The operator passed as argument to the program
  * Return: A pointer to the function
  */
